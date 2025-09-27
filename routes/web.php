13b6d8719be087468604b8af3bf8dd69f601321d
Ian Tyler Applebaum <9451941+ApplebaumIan@users.noreply.github.com>
2025-09-27 23:18:39 +0000

    added the coolest people

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cool-people', function (Request $request) {
+    return json([
+        "ian",
+        "jaimie",
+        "david",
+    ]);
+});
